5144bbe866f6dddc8efbf9cce6a54630fa7ddcb8
darthvader <metricracer@gmail.com>
2017-11-26 09:21:46 -0600

    Massive changes including test scripts

---
+++ check.sh
@@ -0,0 +1,10 @@
+echo "Current LICENSE DIR:"
+ls -ail $LICENSE_DIR
+echo ""
+echo "OLD SAVE DIR:"
+cd $LICENSE_OLD_SAVE 
+ls -ail * 
+echo ""
+echo "NEW SAVE DIR:"
+cd $LICENSE_NEW_SAVE 
+ls -ail * 
